027eb59ea9d41de06ce3d3b5408d4b9afc71e8a6
Kirtiraja Zakheim <kirtiraja@gmail.com>
2019-10-25 15:49:37 -0700

    add var names and values to scripts.js,questions to index.html, intro paragraph, button toggle to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,16 +6,16 @@ $(document).ready(function() {
   $("form#ranking").submit(function(event) {
     event.preventDefault()
   //   $("button").click(function() {
-  // $("p").toggle();
-    var cute1 = $("input:radio[name=cute]:checked").val();
-    var cute2 = $("input:radio[name=cute]:checked").val();
+  // $("#rubytext").toggle();
+    var animal = $("input:radio[name=animal]:checked").val();
+    var color = $("input:radio[name=cute]:checked").val();
     var cute3 = $("input:radio[name=cute]:checked").val();
     var cute4 = $("input:radio[name=cute]:checked").val();
 
-    if (cute1 === "cute1") {
+    if (animal > "1") {
       $(".rubyinfo").show();
       $(".pythoninfo, .jsinfo, .goinfo").hide();
-    } else if (cute2 === "cute2") {
+    } else if (color = "cute2") {
     $(".pythoninfo").show();
       $(".rubyinfo, .jsinfo").hide();
     } else  if (cute3 === "cute3") {
@@ -29,3 +29,4 @@ $(document).ready(function() {
 
 });
 });
+// });
