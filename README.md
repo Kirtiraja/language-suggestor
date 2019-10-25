feedd90ced6f668d8879adaa13051c51c6babb3a
Kirtiraja Zakheim <kirtiraja@gmail.com>
2019-10-25 16:51:05 -0700

    add README.md, and comment out non-working code in scripts.js

---
+++ README.md
@@ -0,0 +1,34 @@
+# _Coding Language Suggestor_
+
+#### _Website with survey questions to suggest which coding language to learn. October 25,2019_
+
+#### By _**Kirtiraja Zakheim **_
+
+## Description
+
+_This website asks the user a few survey questions then once submitted gives information on a coding language.This simple website was created using HTML, Bootstrap and JQuery_
+
+## Setup/Installation Requirements
+
+* _Open file with a web browser of your choice such as Chrome_
+
+
+
+
+## Known Bugs
+
+_Buttons in language headers don't currently toggle/show/hide sub-text yet._
+
+## Support and contact details
+
+_Feel free to let me know why ""#rubytext" doesn't show when clicked_
+
+## Technologies Used
+
+_{Tell me about the languages and tools you used to create this app. Assume that I know you probably used HTML and CSS. If you did something really cool using only HTML, point that out.}_
+
+### License
+
+*{Determine the license under which this application can be used.  See below for more details on licensing.}*
+
+Copyright (c) 2016 **_{List of contributors or company name}_**
--- js/scripts.js
+++ js/scripts.js
@@ -4,26 +4,28 @@
 
 $(document).ready(function() {
   $("form#ranking").submit(function(event) {
-    event.preventDefault()
-  //   $("button").click(function() {
-  // $("#rubytext").toggle();
-    var animal = $("input:radio[name=animal]:checked").val();
-    var color = $("input:radio[name=cute]:checked").val();
-    var cute3 = $("input:radio[name=cute]:checked").val();
-    var cute4 = $("input:radio[name=cute]:checked").val();
+    event.preventDefault();
+    var animal = parseInt($("input:radio[name=animal]:checked").val());
+    var color = parseInt($("input:radio[name=color]:checked").val());
+    var fruit = parseInt($("input:radio[name=fruit]:checked").val());
+    // var cute4 = $("input:radio[name=cute]:checked").val();
 
-    if (animal > "1") {
+    if (animal + color + fruit <= 3) {
       $(".rubyinfo").show();
       $(".pythoninfo, .jsinfo, .goinfo").hide();
-    } else if (color = "cute2") {
+      // $("button").click(function()
+      // $("p#rubytext").show();
+    }
+    else if (animal + color + fruit <= 6) {
     $(".pythoninfo").show();
-      $(".rubyinfo, .jsinfo").hide();
-    } else  if (cute3 === "cute3") {
+      $("#rubytext, .rubyinfo, .jsinfo").hide();
+    }
+    else  {
     $(".jsinfo").show();
     $(".pythoninfo, .goinfo").hide();
-    } else if (cute4 === "cute4") {
-    $(".goinfo").show();
-    $(".pythoninfo, .rubyinfo, .jsinfo").hide();
+    // } else if (cute4 === "cute4") {
+    // $(".goinfo").show();
+    // $(".pythoninfo, .rubyinfo, .jsinfo").hide();
   }
 
 
